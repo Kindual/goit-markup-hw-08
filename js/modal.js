8c493c7765a8c4abed3efe0214103735e58de4b3
Kindual <chubych.viktor@gmail.com>
2022-09-17 23:29:44 +0300

    ho-ho-ho

--- js/modal.js
+++ js/modal.js
@@ -1,29 +1,29 @@
-(() => {
+
     const refs = {
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
     };
-  
+    
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
+
+    document.body.classList.toggle("body--hidden");
     }
-  })();
 
-(() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
 
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+  // const menuBtnRef = document.querySelector("[data-menu-button]");
+  // const mobileMenuRef = document.querySelector("[data-menu]");
 
-    mobileMenuRef.classList.toggle("is-open");
-  });
-})();
+  // menuBtnRef.addEventListener("click", () => {
+  //   const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  //   menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+  
+  //   menuBtnRef.classList.toggle("is-open");
+  //   mobileMenuRef.classList.toggle("is-open");
+  // });
